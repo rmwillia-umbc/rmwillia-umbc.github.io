06163b62535972d97605631a597e99cfe70fa7f2
rmwillia-umbc <128005398+rmwillia-umbc@users.noreply.github.com>
2024-03-02 16:08:53 -0500

    using dist
    sigh

---
+++ script.js
@@ -0,0 +1,32 @@
+var container = document.getElementById("container");
+	var stickyImage = document.getElementById("stickyImage");
+	var tempY;
+	window.addEventListener("scroll", (e) => {
+	//watch how image moves with scrolleing
+		if(window.pageYOffset >= container.offsetTop && window.pageYOffset <= container.offsetTop+container.offsetHeight  - window.innerHeight ){
+			console.log("sliding");
+			stickyImage.style.position = "fixed";
+			stickyImage.style.top = "0vh";
+			stickyImage.style.marginTop = "";
+			tempY = window.pageYOffset;
+		}else if(window.pageYOffset <= container.offsetTop){
+			console.log("top screen");
+			stickyImage.style.position = "absolute";
+			stickyImage.style.top = "";
+			stickyImage.style.marginTop = "";		
+		}else{
+			console.log("stop");
+			stickyImage.style.position = "absolute";
+			stickyImage.style.marginTop = tempY + "px";			
+		}
+	//change images while scroll
+		if(window.pageYOffset < document.getElementById("change1").offsetTop){
+			stickyImage.textContent = "🍓🍓🍓";
+		}
+		if(window.pageYOffset > document.getElementById("change1").offsetTop){
+			stickyImage.textContent = "🍌🍌🍌";
+		}
+		if(window.pageYOffset > document.getElementById("change2").offsetTop){
+			stickyImage.textContent = "🍏🍏🍏";
+		}	
+	})
